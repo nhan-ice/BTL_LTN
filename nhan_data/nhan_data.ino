7a94b6ad6fde7f88b516c69cdc7bf4fcf81cfe69
Phuoc Nhan <lenhan0482005@gmail.com>
2026-04-14 17:40:08 +0700

    init project

--- nhan_data/nhan_data.ino
+++ nhan_data/nhan_data.ino
@@ -7,7 +7,6 @@
 const char* ssid = "Nhan ice";
 const char* password = "12345679";
 
-// Hàm phụ trợ để cắt chuỗi dựa vào dấu phẩy (Ví dụ tách "12,34,56" thành "12", "34", "56")
 String getValue(String data, char separator, int index) {
   int found = 0;
   int strIndex[] = {0, -1};
@@ -26,8 +25,6 @@ String getValue(String data, char separator, int index) {
 void setup() {
   Serial.begin(115200);
   
-  // LƯU Ý: Nếu bên code Arduino bạn đang dùng Serial.begin(9600), 
-  // thì dòng dưới này phải đổi từ 115200 thành 9600 để ESP32 đọc không bị lỗi font nhé!
   Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
   
   Serial.print("Đang kết nối WiFi: ");
@@ -46,13 +43,11 @@ void setup() {
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED && Serial2.available()) {
-    // Đọc dữ liệu từ Arduino gửi sang
     String data = Serial2.readStringUntil('\n');
     data.trim();
     
     // Nếu có dữ liệu
     if (data.length() > 0) {
-      // 1. Tách lấy 7 giá trị từ chuỗi có dấu phẩy do Arduino gửi
       String val_Rel_IAQ  = getValue(data, ',', 0);
       String val_IAQ      = getValue(data, ',', 1);
       String val_TVOC     = getValue(data, ',', 2);
@@ -61,7 +56,6 @@ void loop() {
       String val_Temp     = getValue(data, ',', 5);
       String val_Humidity = getValue(data, ',', 6);
 
-      // 2. Ép vào khuôn JSON để đẩy lên Web
       String payload = "{\"Rel_IAQ\":" + val_Rel_IAQ + 
                        ",\"IAQ\":" + val_IAQ + 
                        ",\"TVOC\":" + val_TVOC + 
@@ -72,9 +66,7 @@ void loop() {
 
       Serial.println("Đang gửi đi JSON: " + payload);
 
-      // 3. Gửi sang Server Python
       HTTPClient http;
-      // NHỚ SỬA LẠI LINK NGROK NẾU NÓ ĐÃ BỊ ĐỔI TÊN MIỀN MỚI
       http.begin("https://unresigned-monopodially-fermina.ngrok-free.dev/update");
       http.addHeader("Content-Type", "application/json"); // Đã chuyển thành json
       
